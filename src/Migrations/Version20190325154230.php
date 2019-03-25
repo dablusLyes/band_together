4553ad68c571cf1d772f70308a13014fd07a4c15
unknown <lyes.dabla@gmail.com>
2019-03-25 16:57:40 +0100

    fin du bakofisse

--- src/Migrations/Version20190325154230.php
+++ src/Migrations/Version20190325154230.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190325104102 extends AbstractMigration
+final class Version20190325154230 extends AbstractMigration
 {
     public function getDescription() : string
     {
