b18e8e8b42b622b4d7d796b3adf507dc163d7adb
Remi Molinero <contact@remimolinero.com>
2019-03-25 09:58:21 +0100

    fix migrations, add templates+routes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,12 +8,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default")
+     * @Route("/", name="home")
      */
     public function index()
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/news", name="news")
+     */
+    public function news()
+    {
+        return $this->render('default/news.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('default/contact.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/apropos", name="about")
+     */
+    public function about()
+    {
+        return $this->render('default/about.html.twig', [
+
         ]);
     }
 }
--- src/Migrations/Version20190325085253.php
+++ src/Migrations/Version20190325085253.php
@@ -10,11 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<< HEAD:src/Migrations/Version20190322144716.php
-final class Version20190322144716 extends AbstractMigration
-=======
-final class Version20190322114445 extends AbstractMigration
->>>>>>> 48e7372d0a8e0a9182ec473aafc99dd1daa6c98a:src/Migrations/Version20190322114445.php
+final class Version20190325085253 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -27,10 +23,7 @@ final class Version20190322114445 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE instrument (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, categorie VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-<<<<<<< HEAD:src/Migrations/Version20190322144716.php
-=======
         $this->addSql('CREATE TABLE style (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, categoerie VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
->>>>>>> 48e7372d0a8e0a9182ec473aafc99dd1daa6c98a:src/Migrations/Version20190322114445.php
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
@@ -40,10 +33,7 @@ final class Version20190322114445 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE instrument');
-<<<<<<< HEAD:src/Migrations/Version20190322144716.php
-=======
         $this->addSql('DROP TABLE style');
->>>>>>> 48e7372d0a8e0a9182ec473aafc99dd1daa6c98a:src/Migrations/Version20190322114445.php
         $this->addSql('DROP TABLE user');
     }
 }
