9f1487d843b1975ef4ffb166f6aaf8c0f872e9fa
quidelantoine <quidelantoine@gmail.com>
2019-03-28 12:36:18 +0100

    creation page profil

--- src/Controller/UserFrontController.php
+++ src/Controller/UserFrontController.php
@@ -2,18 +2,43 @@
 
 namespace App\Controller;
 
+use App\Form\ProfilType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserFrontController extends AbstractController
 {
     /**
-     * @Route("/user/front", name="user_front")
+     * @Route("/profil", name="profil")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('user_front/index.html.twig', [
-            'controller_name' => 'UserFrontController',
+
+
+        $user = $this->getUser();
+        //dd($user);
+        if(!$user) {
+            throw $this->createNotFoundException('The user does not exist');
+        }
+
+        $form = $this->createForm(ProfilType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Your changes were saved!'
+            );
+            return $this->redirectToRoute('profil');
+        }
+        return $this->render('profil/index.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
         ]);
     }
 }
