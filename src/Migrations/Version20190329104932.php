cc617eaa60f7ece33baa667ae2484633e925c8ba
Remi Molinero <contact@remimolinero.com>
2019-03-29 15:47:38 +0100

    integration

--- src/Migrations/Version20190329104932.php
+++ src/Migrations/Version20190329104932.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190329092901 extends AbstractMigration
+final class Version20190329104932 extends AbstractMigration
 {
     public function getDescription() : string
     {
