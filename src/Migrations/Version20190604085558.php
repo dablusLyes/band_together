ef08ed8656adde7a1a0555f24cc83bc4aa05abe0
RemiMolinero <contact@remimolinero.com>
2019-06-04 10:57:37 +0200

    Proper migration file

--- src/Migrations/Version20190604085558.php
+++ src/Migrations/Version20190604085558.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190604082900 extends AbstractMigration
+final class Version20190604085558 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -27,8 +27,6 @@ final class Version20190604082900 extends AbstractMigration
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, departement_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, token VARCHAR(255) NOT NULL, username VARCHAR(100) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), INDEX IDX_8D93D649CCF9E01E (departement_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_instruments (user_id INT NOT NULL, instruments_id INT NOT NULL, INDEX IDX_4A4B179EA76ED395 (user_id), INDEX IDX_4A4B179E1EC49357 (instruments_id), PRIMARY KEY(user_id, instruments_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_user (user_source INT NOT NULL, user_target INT NOT NULL, INDEX IDX_F7129A803AD8644E (user_source), INDEX IDX_F7129A80233D34C1 (user_target), PRIMARY KEY(user_source, user_target)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE departements_user ADD CONSTRAINT FK_357A0F9A1DB279A6 FOREIGN KEY (departements_id) REFERENCES departements (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE departements_user ADD CONSTRAINT FK_357A0F9AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649CCF9E01E FOREIGN KEY (departement_id) REFERENCES departements (id)');
         $this->addSql('ALTER TABLE user_instruments ADD CONSTRAINT FK_4A4B179EA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_instruments ADD CONSTRAINT FK_4A4B179E1EC49357 FOREIGN KEY (instruments_id) REFERENCES instruments (id) ON DELETE CASCADE');
